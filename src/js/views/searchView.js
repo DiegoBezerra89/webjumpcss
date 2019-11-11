b31f3622c4378a9ce667125ca791be03efae1c59
Diego Bezerra <diegobmartins21@gmail.com>
2019-11-11 17:26:40 -0300

    All the responsive layout was implemmented, I was so inserted on the job that I forget to commit.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -69,7 +69,7 @@ const renderItem = item => {
             </figure>
             <div class="card__text">
                 <h4 class="card__text--name">${name}</h4>
-                <p class="card__text--price u-margin-top big-title">R$ ${price}</p>
+                <p class="card__text--price big-title">R$ ${price}</p>
             </div>
             <button class="card__button">Comprar</button>
         </div>
